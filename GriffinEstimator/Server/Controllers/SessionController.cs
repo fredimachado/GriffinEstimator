13e57217e8be853f7e0798554566a66558f00d58
Fredi Machado <fredisoft@gmail.com>
2023-08-19 17:40:17 +1000

    Add basic logging

--- GriffinEstimator/Server/Controllers/SessionController.cs
+++ GriffinEstimator/Server/Controllers/SessionController.cs
@@ -8,25 +8,36 @@ namespace GriffinEstimator.Server.Controllers;
 public class SessionController : ControllerBase
 {
     private readonly SessionManager _sessionManager;
+    private readonly ILogger _logger;
 
-    public SessionController(SessionManager sessionManager)
+    public SessionController(SessionManager sessionManager, ILogger<SessionController> logger)
     {
         _sessionManager = sessionManager;
+        _logger = logger;
     }
 
     [HttpPost("join")]
     public IActionResult Join([FromBody] JoinRequest request)
     {
+        if (string.IsNullOrWhiteSpace(request.SessionId) || string.IsNullOrWhiteSpace(request.MemberName))
+        {
+            return BadRequest();
+        }
+
         try
         {
             var session = _sessionManager.GetSession(request.SessionId);
+
+            _logger.LogInformation("Found session {sessionId} for Member {memberName}", session, request.MemberName);
+
             return Ok(request.SessionId);
         }
         catch
         {
+            _logger.LogWarning("Member {memberName} tried to join an invalid session ({sessionId})", request.MemberName, request.SessionId);
             return NotFound();
         }
     }
 }
 
-public record JoinRequest(string SessionId);
+public record JoinRequest(string SessionId, string MemberName);
--- GriffinEstimator/Server/Hubs/PokerSessionHub.cs
+++ GriffinEstimator/Server/Hubs/PokerSessionHub.cs
@@ -9,23 +9,31 @@ public class PokerSessionHub : Hub<IPokerSession>
 {
     private readonly SessionManager _sessionManager;
     private readonly IOptionsMonitor<GriffinEstimatorSettings> _options;
+    private readonly ILogger _logger;
 
-    public PokerSessionHub(SessionManager sessionManager, IOptionsMonitor<GriffinEstimatorSettings> options)
+    public PokerSessionHub(
+        SessionManager sessionManager,
+        IOptionsMonitor<GriffinEstimatorSettings> options,
+        ILogger<PokerSessionHub> logger)
     {
         _sessionManager = sessionManager;
         _options = options;
+        _logger = logger;
     }
 
     public async Task<string> StartSession(string secretKey)
     {
         if (secretKey != _options.CurrentValue.SecretKey)
         {
+            _logger.LogError("Host tried to start a session with an invalid key: '{key}'", secretKey);
             return "error";
         }
 
         var session = _sessionManager.CreateSession(Context.ConnectionId);
 
         await Groups.AddToGroupAsync(Context.ConnectionId, session.Id);
+
+        _logger.LogInformation("Host has started a new session: '{sessionId}'", session.Id);
 
         return session.Id;
     }
@@ -39,6 +47,8 @@ public class PokerSessionHub : Hub<IPokerSession>
 
         await Clients.Group(sessionId)
                      .MemberJoined(memberName);
+
+        _logger.LogInformation("Member '{memberName}' has joined a session: '{sessionId}'", memberName, sessionId);
 
         if (session.CurrentState == SessionState.RoundInProgress)
         {
@@ -56,6 +66,8 @@ public class PokerSessionHub : Hub<IPokerSession>
 
         await Clients.Group(sessionId)
                      .EstimateSubmitted(memberName);
+
+        _logger.LogInformation("Member '{memberName}' has submitted estimation for session: '{sessionId}'", memberName, sessionId);
     }
 
     public async Task StartRound(string sessionId)
@@ -63,6 +75,7 @@ public class PokerSessionHub : Hub<IPokerSession>
         var session = _sessionManager.GetSession(sessionId);
         if (session.HostConnectionId != Context.ConnectionId)
         {
+            _logger.LogError("Invalid connection tried to start round of session: '{sessionId}'", sessionId);
             return;
         }
 
@@ -70,6 +83,8 @@ public class PokerSessionHub : Hub<IPokerSession>
 
         await Clients.Group(sessionId)
                      .RoundStarted();
+
+        _logger.LogInformation("Host has started a round for session: '{sessionId}'", sessionId);
     }
 
     public async Task RestartRound(string sessionId)
@@ -77,6 +92,7 @@ public class PokerSessionHub : Hub<IPokerSession>
         var session = _sessionManager.GetSession(sessionId);
         if (session.HostConnectionId != Context.ConnectionId)
         {
+            _logger.LogError("Invalid connection tried to restart round of session: '{sessionId}'", sessionId);
             return;
         }
 
@@ -84,6 +100,8 @@ public class PokerSessionHub : Hub<IPokerSession>
 
         await Clients.Group(sessionId)
                      .RoundStarted();
+
+        _logger.LogInformation("Host has restarted a round for session: '{sessionId}'", sessionId);
     }
 
     public async Task EndRound(string sessionId)
@@ -91,6 +109,7 @@ public class PokerSessionHub : Hub<IPokerSession>
         var session = _sessionManager.GetSession(sessionId);
         if (session.HostConnectionId != Context.ConnectionId)
         {
+            _logger.LogError("Invalid connection tried to end round of session: '{sessionId}'", sessionId);
             return;
         }
 
@@ -98,5 +117,7 @@ public class PokerSessionHub : Hub<IPokerSession>
         
         await Clients.Group(sessionId)
                      .RoundEnded(result);
+
+        _logger.LogInformation("Host has ended a round for session: '{sessionId}'", sessionId);
     }
 }
