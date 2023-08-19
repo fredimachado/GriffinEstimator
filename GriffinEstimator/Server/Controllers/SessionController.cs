cc841b8549793d7d57f08f91ce465cdcf8c6116e
Fredi Machado <fredisoft@gmail.com>
2023-08-19 17:00:37 +1000

    Add join endpoint to check for existing session

---
+++ GriffinEstimator/Server/Controllers/SessionController.cs
@@ -0,0 +1,32 @@
+﻿using GriffinEstimator.Server.Session;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GriffinEstimator.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SessionController : ControllerBase
+{
+    private readonly SessionManager _sessionManager;
+
+    public SessionController(SessionManager sessionManager)
+    {
+        _sessionManager = sessionManager;
+    }
+
+    [HttpPost("join")]
+    public IActionResult Join([FromBody] JoinRequest request)
+    {
+        try
+        {
+            var session = _sessionManager.GetSession(request.SessionId);
+            return Ok(request.SessionId);
+        }
+        catch
+        {
+            return NotFound();
+        }
+    }
+}
+
+public record JoinRequest(string SessionId);
