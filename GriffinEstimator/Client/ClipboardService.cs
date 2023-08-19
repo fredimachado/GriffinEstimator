1bfd734fdcf6def1a5f5ca4b39b74c39d3f71ed2
Fredi Machado <fredisoft@gmail.com>
2023-08-19 17:03:03 +1000

    Allow copy link to join session

---
+++ GriffinEstimator/Client/ClipboardService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace GriffinEstimator.Client;
+
+public class ClipboardService
+{
+    private readonly IJSRuntime _jsInterop;
+
+    public ClipboardService(IJSRuntime jsInterop)
+    {
+        _jsInterop = jsInterop;
+    }
+
+    public async Task CopyToClipboard(string text)
+    {
+        await _jsInterop.InvokeVoidAsync("navigator.clipboard.writeText", text);
+    }
+}
--- GriffinEstimator/Client/Program.cs
+++ GriffinEstimator/Client/Program.cs
@@ -9,6 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddScoped<ClipboardService>();
+
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<BrowserStorage>();
 
